4622b782668aa75ef54e59d68bdf52d303dfcd38
Virginia Wordsworth <vwordsworth@gmail.com>
2020-12-04 10:21:23 -0500

    Refactor validation expressions

--- solutions/2020/day4/solution.go
+++ solutions/2020/day4/solution.go
@@ -8,10 +8,8 @@ import (
 	"regexp"
 )
 
-type entry struct {
-	value      string
-	letter     byte
-	int1, int2 int
+type validation struct {
+	byr, iyr, eyr, hgt, hcl, ecl, pid *regexp.Regexp
 }
 
 func getInput() []string {
@@ -23,7 +21,6 @@ func getInput() []string {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		currentLine := scanner.Text()
-
 		if len(currentLine) == 0 {
 			lines = append(lines, line.String())
 			line.Reset()
@@ -40,49 +37,52 @@ func getInput() []string {
 	return lines
 }
 
-func processPassports(passports []string) (int, int) {
+func newFieldValidation() *validation {
+	v := validation{}
+	v.byr, _ = regexp.Compile("\\s*(byr:)[#\\w]+\\s*")
+	v.iyr, _ = regexp.Compile("\\s*(iyr:)[#\\w]+\\s*")
+	v.eyr, _ = regexp.Compile("\\s*(eyr:)[#\\w]+\\s*")
+	v.hgt, _ = regexp.Compile("\\s*(hgt:)[#\\w]+\\s*")
+	v.hcl, _ = regexp.Compile("\\s*(hcl:)[#\\w]+\\s*")
+	v.ecl, _ = regexp.Compile("\\s*(ecl:)[#\\w]+\\s*")
+	v.pid, _ = regexp.Compile("\\s*(pid:)[#\\w]+\\s*")
+	return &v
+}
+
+func newFieldAndContentValidation() *validation {
+	v := validation{}
+	v.byr, _ = regexp.Compile("\\s*(byr:)((200)[0-2]|(19)[2-9][0-9])\\s*")
+	v.iyr, _ = regexp.Compile("\\s*(iyr:)((201)[0-9]|2020)\\s*")
+	v.eyr, _ = regexp.Compile("\\s*(eyr:)((202)[0-9]|2030)\\s*")
+	v.hgt, _ = regexp.Compile("\\s*(hgt:)((1[5-8][0-9]|19[0-3])(cm)|(59|6[0-9]|7[0-6])(in))\\s*")
+	v.hcl, _ = regexp.Compile("\\s*(hcl:)#[0-9a-f]{6}\\s*")
+	v.ecl, _ = regexp.Compile("\\s*(ecl:)(amb|blu|brn|gry|grn|hzl|oth)\\s*")
+	v.pid, _ = regexp.Compile("\\s*(pid:)\\d{9}($|\\s+)")
+	return &v
+}
+
+func countValidPassports(passports []string, rules *validation) int {
 	count := 0
-	countWithRules := 0
 	for _, pass := range passports {
-		if isPassportValid(pass) {
+		if isPassportValid(pass, rules) {
 			count++
-			if isPassportValidWithRules(pass) {
-				countWithRules++
-			}
 		}
 	}
-
-	return count, countWithRules
+	return count
 }
 
-func isPassportValid(pass string) bool {
-	byr, _ := regexp.Compile("\\s*(byr:)[#\\w]+\\s*")
-	iyr, _ := regexp.Compile("\\s*(iyr:)[#\\w]+\\s*")
-	eyr, _ := regexp.Compile("\\s*(eyr:)[#\\w]+\\s*")
-	hgt, _ := regexp.Compile("\\s*(hgt:)[#\\w]+\\s*")
-	hcl, _ := regexp.Compile("\\s*(hcl:)[#\\w]+\\s*")
-	ecl, _ := regexp.Compile("\\s*(ecl:)[#\\w]+\\s*")
-	pid, _ := regexp.Compile("\\s*(pid:)[#\\w]+\\s*")
-
-	return byr.MatchString(pass) && iyr.MatchString(pass) && eyr.MatchString(pass) && hgt.MatchString(pass) && hcl.MatchString(pass) && ecl.MatchString(pass) && pid.MatchString(pass)
-}
-
-func isPassportValidWithRules(pass string) bool {
-	byr, _ := regexp.Compile("\\s*(byr:)((200)[0-2]|(19)[2-9][0-9])\\s*")
-	iyr, _ := regexp.Compile("\\s*(iyr:)((201)[0-9]|2020)\\s*")
-	eyr, _ := regexp.Compile("\\s*(eyr:)((202)[0-9]|2030)\\s*")
-	hgt, _ := regexp.Compile("\\s*(hgt:)((1[5-8][0-9]|19[0-3])(cm)|(59|6[0-9]|7[0-6])(in))\\s*")
-	hcl, _ := regexp.Compile("\\s*(hcl:)#[0-9a-f]{6}\\s*")
-	ecl, _ := regexp.Compile("\\s*(ecl:)(amb|blu|brn|gry|grn|hzl|oth)\\s*")
-	pid, _ := regexp.Compile("\\s*(pid:)\\d{9}($|\\s+)")
-
-	return byr.MatchString(pass) && iyr.MatchString(pass) && eyr.MatchString(pass) && hgt.MatchString(pass) && hcl.MatchString(pass) && ecl.MatchString(pass) && pid.MatchString(pass)
+func isPassportValid(pass string, rules *validation) bool {
+	return rules.byr.MatchString(pass) && rules.iyr.MatchString(pass) && rules.eyr.MatchString(pass) && rules.hgt.MatchString(pass) && rules.hcl.MatchString(pass) && rules.ecl.MatchString(pass) && rules.pid.MatchString(pass)
 }
 
 func main() {
 	input := getInput()
 
-	count, countWithRules := processPassports(input)
-	fmt.Println("Passport count:\t", count)
-	fmt.Println("Passport count with rules:\t", countWithRules)
+	fieldRules := newFieldValidation()
+	count := countValidPassports(input, fieldRules)
+	fmt.Println("Valid fields:\t", count)
+
+	contentRules := newFieldAndContentValidation()
+	countWithRules := countValidPassports(input, contentRules)
+	fmt.Println("Valid fields & contents:\t", countWithRules)
 }
