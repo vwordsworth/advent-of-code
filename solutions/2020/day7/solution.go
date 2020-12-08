44e4b36d30b27a3077d39b0fe24a22e686dd0341
Virginia Wordsworth <vwordsworth@gmail.com>
2020-12-07 21:58:34 -0500

    Improve naming clarity and readability

--- solutions/2020/day7/solution.go
+++ solutions/2020/day7/solution.go
@@ -8,6 +8,12 @@ import (
 	"strconv"
 )
 
+type inputLine struct {
+	bagName string
+	contentNames []string
+	quantities []int
+}
+
 type bag struct {
 	name      string
 	contents     []*inventory
@@ -16,6 +22,20 @@ type bag struct {
 type inventory struct {
 	bagStock *bag
 	quantity int
+}
+
+func newInputLine(line string) *inputLine {
+	i := inputLine{}
+	nameRegEx := regexp.MustCompile(`(?P<name>[a-zA-Z\s]+) bags contain `)
+	i.bagName = nameRegEx.FindStringSubmatch(line)[1]
+
+	contentRegEx := regexp.MustCompile(`(?P<quantity>\d)\s(?P<content>[a-zA-Z\s]+) bags?`)
+	for _, content := range contentRegEx.FindAllStringSubmatch(line, -1) {
+		quantity, _ := strconv.Atoi(content[1])
+		i.quantities = append(i.quantities, quantity)
+		i.contentNames = append(i.contentNames, content[2])
+	}
+	return &i
 }
 
 func newBag(name string, contents []*inventory) *bag {
@@ -39,51 +59,33 @@ func getInput() map[string]*bag {
 	bags := make(map[string]*bag)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		bagName, contentNames, quantities := parseBagLine(scanner.Text())
-		var contents []*inventory
-		for i, content := range contentNames {
+		input := newInputLine(scanner.Text())
+
+		var childInventory []*inventory
+		for i, childName := range input.contentNames {
 			var b *bag
-			if _, exists := bags[content]; exists {
-				b = bags[content]
+			if _, exists := bags[childName]; exists {
+				b = bags[childName]
 			} else {
-				b = newBag(content, nil)
-				bags[content] = b
+				b = newBag(childName, nil)
+				bags[childName] = b
 			}
-			inv := newInventory(b, quantities[i])
-			contents = append(contents, inv)
+			inv := newInventory(b, input.quantities[i])
+			childInventory = append(childInventory, inv)
 		}
 
-		if _, hasBag := bags[bagName]; hasBag {
-			b := bags[bagName]
-			b.contents = append(b.contents, contents...)
+		if existingBag, hasBag := bags[input.bagName]; hasBag {
+			existingBag.contents = append(existingBag.contents, childInventory...)
 		} else {
-			bags[bagName] = newBag(bagName, contents)
+			bags[input.bagName] = newBag(input.bagName, childInventory)
 		}
 	}
 	return bags
 }
 
-func parseBagLine(line string) (string, []string, []int) {
-	nameRegEx := regexp.MustCompile(`(?P<name>[a-zA-Z\s]+) bags contain `)
-	bagName := nameRegEx.FindStringSubmatch(line)[1]
-
-	contentRegEx := regexp.MustCompile(`(?P<quantity>\d)\s(?P<content>[a-zA-Z\s]+) bags?`)
-	var contentNames []string
-	var quantities []int
-
-	for _, content := range contentRegEx.FindAllStringSubmatch(line, -1) {
-		quantity, _ := strconv.Atoi(content[1])
-		quantities = append(quantities, quantity)
-		contentNames = append(contentNames, content[2])
-	}
-
-	return bagName, contentNames, quantities
-}
-
 func numberContainingColor(arrangement map[string]*bag, color string) int {
 	searched := make(map[string]bool)
 	count := 0
-
 	for name, b := range arrangement {
 		doesContain := containsColor(b, color)
 		if _, isPresent := searched[name]; doesContain && !isPresent {
@@ -91,21 +93,18 @@ func numberContainingColor(arrangement map[string]*bag, color string) int {
 			searched[name] = true
 		}
 	}
-
 	return count
 }
 
 func containsColor(b *bag, color string) bool {
-	if b == nil || b.contents == nil {
-		return false
-	} else {
+	if b.contents != nil {
 		for _, child := range b.contents {
 			if child.bagStock.name == color || containsColor(child.bagStock, color) {
 				return true
 			}
 		}
-		return false
 	}
+	return false
 }
 
 func countUnderColor(arrangement map[string]*bag, root string) int {
@@ -113,15 +112,11 @@ func countUnderColor(arrangement map[string]*bag, root string) int {
 }
 
 func countTree(b *bag) int {
-	if len(b.contents) == 0 {
-		return 1
-	} else {
-		childCount := 0
-		for _, child := range b.contents {
-			childCount += countTree(child.bagStock)*child.quantity
-		}
-		return 1 + childCount
+	count := 1
+	for _, child := range b.contents {
+		count += countTree(child.bagStock)*child.quantity
 	}
+	return count
 }
 
 func main() {
